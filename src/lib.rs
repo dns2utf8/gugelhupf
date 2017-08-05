12f4e760960ea6564f4ec2b338a1ff2339c89d3f
Stefan Schindler <dns2utf8@estada.ch>
2017-08-05 15:17:37 +0200

    Implement some basig types from EBNF

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate nom;
 
 mod parser;
 
+/// The global context of this VM
 pub struct Gugelhupf {
     global_name: String,
     objects: Vec<JsObject>,
@@ -20,7 +21,13 @@ impl Gugelhupf {
         self
     }
 
-    pub fn has_function<S>(&self, name: S) -> bool where S: Into<String> {
+    /// Check if a globally accessable function is registered
+    ///
+    /// If the property does not start with the global_name the function
+    /// will prefix it
+    pub fn has_function(&self, name: &str) -> bool {
+        if name.starts_with(&*self.global_name) == false {
+        }
         false
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,27 +1,78 @@
 use super::*;
 
 
-named!(undefined<Keyword>, do_parse!(
+
+#[derive(Eq,PartialEq,Debug,Copy,Clone)]
+enum Keyword {
+    Undefined, Null,
+    Var, Let, Const,
+    Function,
+    If, While, For,
+}
+
+named!(undefined_lit<Keyword>, do_parse!(
     tag!("undefined") >>
     (Keyword::Undefined)
     ));
-
-named!(function<Keyword>, do_parse!(
-    tag!("function") >>
-    (Keyword::Function)
-    ));
-
-named!(null<Keyword>, do_parse!(
+named!(null_lit<Keyword>, do_parse!(
     tag!("null") >>
     (Keyword::Null)
     ));
 
+named!(var_lit<Keyword>, do_parse!(
+    tag!("var") >>
+    (Keyword::Var)
+    ));
+named!(let_lit<Keyword>, do_parse!(
+    tag!("let") >>
+    (Keyword::Let)
+    ));
+named!(const_lit<Keyword>, do_parse!(
+    tag!("const") >>
+    (Keyword::Const)
+    ));
+
+named!(function_lit<Keyword>, do_parse!(
+    tag!("function") >>
+    (Keyword::Function)
+    ));
+
+named!(if_lit<Keyword>, do_parse!(
+    tag!("if") >>
+    (Keyword::If)
+    ));
+named!(if_while<Keyword>, do_parse!(
+    tag!("while") >>
+    (Keyword::While)
+    ));
+named!(for_lit<Keyword>, do_parse!(
+    tag!("for") >>
+    (Keyword::For)
+    ));
+
+
 #[derive(Eq,PartialEq,Debug,Copy,Clone)]
-enum Keyword {
-    Undefined,
-    Null,
-    Function,
+enum Operator {
+    Plus, Minus, Mul, Div
 }
+
+named!(plus_op<Operator>, do_parse!(
+    tag!("+") >>
+    (Operator::Plus)
+    ));
+named!(minus_op<Operator>, do_parse!(
+    tag!("-") >>
+    (Operator::Minus)
+    ));
+named!(mul_op<Operator>, do_parse!(
+    tag!("*") >>
+    (Operator::Mul)
+    ));
+named!(div_op<Operator>, do_parse!(
+    tag!("/") >>
+    (Operator::Div)
+    ));
+
 
 #[cfg(test)]
 mod test {
@@ -37,7 +88,7 @@ mod test {
     #[test]
     fn parse_undefined_literal() {
         let src = "undefined";
-        let ast = undefined(src.as_bytes());
+        let ast = undefined_lit(src.as_bytes());
 
         assert_eq!(Keyword::Undefined, ast.unwrap().1);
     }
@@ -45,7 +96,7 @@ mod test {
     #[test]
     fn parse_function_literal() {
         let src = "function";
-        let ast = function(src.as_bytes());
+        let ast = function_lit(src.as_bytes());
 
         assert_eq!(Keyword::Function, ast.unwrap().1);
     }
@@ -53,7 +104,7 @@ mod test {
     #[test]
     fn parse_null_literal() {
         let src = "null";
-        let ast = null(src.as_bytes());
+        let ast = null_lit(src.as_bytes());
 
         assert_eq!(Keyword::Null, ast.unwrap().1);
     }
